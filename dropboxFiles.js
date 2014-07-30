1ece7d29a567ccfeceb16024f51e7677c5b2750a
lathropd <daniel.lathrop@gmail.com>
2014-07-30 09:42:47 -0500

    dropbox populates name list

--- dropboxFiles.js
+++ dropboxFiles.js
@@ -1,42 +1,46 @@
 var directory;
 var client;
+var skybox;
+var form;
+var O;
+var ul;
 
 $(document).ready( function () {
 
 
- client = new Dropbox.Client({key: 'phovkvef0jhkx4i'});
+ 	client = new Dropbox.Client({key: 'phovkvef0jhkx4i'});
 
 
-client.onError.addListener(function(error) { alert("there's been an error. please reload the page.\n\n"+error); client.reset();});
+	client.onError.addListener(function(error) { alert("there's been an error. please reload the page.\n\n"+error); client.reset();});
 
 
-client.authenticate();
-
-//client.makeUrl('hello_world.txt', {downloadHack: true}, function(a,b) { console.log(a); console.log(b);});
-
-
-
-client.readdir("/", function(error, entries) {
-  if (error) {
-  	alert('there has been an error: ' + error + ' try refreshing');
-  	client.reset();
-    return false;  // Something went wrong.
-  }
-  
-	directory = entries;
-  //alert("Your Dropbox contains " + entries.join(", "));
+	client.authenticate();
+	//client.makeUrl('hello_world.txt', {downloadHack: true}, function(a,b) { console.log(a); console.log(b);});
+	form = $("#adminForm");
+	skybox = "#skybox";
+	$(skybox).html('<select style="height: 50px; width: 200px;" id="skyboxList" multiple="true"></select><div id="skyboxMessage"></div>');
+	O = obit(client, form, skybox);	
 });
 
-});
 
 function obit(client, form, skybox) {
 	this.data = {};
 	//this.user = client.
 	this.entries = [];
 	this.loadEntries = function() {
-		client.readdir("/", function(error, entries) {
+		client.readdir("/", function (error, entries) {
 			this.entries = entries;
-		})
+			var skyboxList = skybox + " #skyboxList";
+			$(skyboxList).html();
+			for (entry in entries) {
+				if (entries[entry].match(/\.json$/)) {
+					$(skyboxList).append("<option class='filename' id='"+entries[entry]+"'>"+entries[entry]+"</option>");
+					
+				}				
+			}
+			$("option.filename").click(function (el) {console.log(el)});
+
+		});
 	};
 	this.create = function() {
 		this.data = {};
@@ -51,8 +55,11 @@ function obit(client, form, skybox) {
 		this.populate_form();
 	};
 	
+	
+	
 	this.populate_form = function () {
 		this_obit = this;
+		
 		$(form.children).each(function (i, el) {
 				if (this_obit.data.hasOwnProperty(el.id)) {
 					$(el).val(this_obit.data[el.id]);
@@ -65,7 +72,6 @@ function obit(client, form, skybox) {
 	this.json = function () { return JSON.stringify(this.data);};
 	this.write = function() {
 		this.client.write(this.data.file_name, this.json());
-		this.loadEntries();
 	}
 	this.loadEntries();
 	return this;
