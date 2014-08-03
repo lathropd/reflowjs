64bddb984379bdc6b5c88e29c5552ca01b69259c
lathropd <daniel.lathrop@gmail.com>
2014-08-02 22:12:33 -0500

    fixing index.html links to  vendor js files that were moved a second time

--- static/js/admin.js
+++ static/js/admin.js
@@ -104,7 +104,7 @@ function obit(client, form, skybox) {
     this.load = function (name) {
         this_obit = this;
         form.find("input, textarea").val("");
-        form.find("a#url, a#webUrl").text("").prop("href","");
+        form.find("a#url, a#webUrl").text("this will populate automatically").prop("href","");
         $("button#reset").click();
         this.data.file_name = name;
         client.readFile(name, function (error, results) {
