90a03bf301054d9f65eceab1996680ffbe50461b
lathropd <daniel.lathrop@gmail.com>
2014-08-14 23:35:53 -0500

    Revert "Revert "changed obit() to admin, changed dropbox to use a folder rather than an app folder""
    This restores commit 0042c5550552eb9aab223e12300197e08b0bbf02

--- static/js/admin.js
+++ static/js/admin.js
@@ -8,20 +8,20 @@ var skyboxHTML;
 
 var elementtest;
 $(document).ready( function () {
-    client = new Dropbox.Client({key: 'phovkvef0jhkx4i', token:'zy9Bs1eG-scAAAAAAAABQywH6g14oaZPKxItX9tMUbQ-D7ca2fJ3_9p6iEBwOY-g'});
-
-    //client.onError.addListener(function(error) { alert("there's been an error. please reload the page.\n\n"+error); client.reset();});
+    client = new Dropbox.Client({key: 'adhp94h23c2mapg'});
+    client.reset();
     client.authenticate();
-    //client.makeUrl('hello_world.txt', {downloadHack: true}, function(a,b) { console.log(a); console.log(b);});
     form = $("#adminForm");
     form[0].reset();
-    skyboxHTML =$(skybox).html();
-
+    skyboxHTML = $(skybox).html();
     $(skybox).html(skyboxHTML);
-    O = obit(client, form, skybox);
+    O = admin({client: client, form:form, skybox:skybox, folder: 'AJC-Reflow'});
     $("button.create").click(function(e){O.create();return false;});
     $("button.save").click(function(e){O.save();return false;});
+    $("button.rename").click(function(e){O.rename();return false;});
     $("button.reset").click(function(e){O.data.file_name ="";});
+    $("textarea#tableGenerator").keyup(function () { textToTable($("textarea#tableGenerator"), $("#tableCode"));});
+	$("textarea#tableGenerator").keyup();
 
 });
 
@@ -43,19 +43,47 @@ function textToTable(element, target) {
     target.text(html);
 }
 
-$("textarea#tableGenerator").keyup(function () { textToTable($("textarea#tableGenerator"), $("#tableCode"));});
-$("textarea#tableGenerator").keyup();
 
 
-function obit(client, form, skybox) {
-    this.data = {};
-    this.form = form;
-    //this.user = client.
+// create a JS object by having a function return itself
+function admin( options ) {
+
+	//set default options
+	this.options = {
+		client: null,
+		form: '#adminForm',
+		skybox: '#skyBoxForm',
+		folder: '',
+		drafts: '/Drafts',
+		published: '/Published',
+		data: {}
+	}
+	
+	// pass in the actual options
+	$.extend(this.options, options);
+	
+	// create a local variable for the client
+	client = this.options.client;
+	
+	// load initial data (normally blank)
+    this.data = this.options.data;
+    
+    // load the form selector
+    this.form = this.options.form;
+    
+    // start with empty list of entries
     this.entries = [];
+    
+    
+    // define the object's methods
+    // (technically these are attributes containing functions)
+    
+    // load entries
     this.loadEntries = function() {
-        this_obit = this;
+    	// create a variable we can pass to callbacks
+        this_admin = this;
         $(skybox).html(skyboxHTML);
-        client.readdir("/", function (error, entries) {
+        client.readdir("/AJC-Reflow/Drafts", function (error, entries) {
             this.entries = entries;
             var skyboxList = skybox + " #skyboxList";
             $(skyboxList).html();
@@ -63,22 +91,26 @@ function obit(client, form, skybox) {
                 if (entries[entry].match(/\.json$/)) {
                     idString  = entries[entry].replace(/\.json$/,'');
                     name = idString.replace(/[\W_]/g, ' ');
-                    if (entries[entry] == this.data.file_name) {
+                    if ('/AJC-Reflow/Drafts/' + entries[entry] == this.data.file_name) {
                         $(skyboxList).append("<option class='filename' id='"+idString+"' selected='selected'>"+name+"</option>");
                     } else {
                         $(skyboxList).append("<option class='filename' id='"+idString+"'>"+name+"</option>");
                     }
                 }
             }
-            $("option.filename").on("click", function (event) {this_obit.load(this.id+".json")});
+            $("option.filename").on("click", function (event) {this_admin.load(this.id+".json")});
             $("#"+(this.data.file_name||"nothing").replace(/.json/,"")).click();
         });
     };
+    
+    // create 
     this.create = function() {
+        // create a variable we can pass to callbacks
+        this_admin = this;
         console.log(1)
             this.data = {};
         this.data.name = window.prompt("Enter name of a new subject","");
-        this.data.file_name = this.data.name.toLowerCase().replace(/\W+/g, '_') + '.json';
+        this.data.file_name = '/AJC-Reflow/Drafts/' + this.data.name.toLowerCase().replace(/\W+/g, '_') + '.json';
         if (this.entries.indexOf(this.data.file_name)>-1) {
             //silently fail, keeping the old one
         } else {
@@ -87,20 +119,24 @@ function obit(client, form, skybox) {
     };
 
     this.load = function (name) {
-        this_obit = this;
+    	// create a variable we can pass to callbacks
+        this_admin = this;
         form.find("input, textarea").val("");
         form.find("a#url, a#webUrl").text("this will populate automatically").prop("href","");
         $("button#reset").click();
-        this.data.file_name = name;
-        client.readFile(name, function (error, results) {
+        this.data.file_name = '/AJC-Reflow/Drafts/' + name;
+        thisFileName = this.data.file_name;
+        client.readFile(this.data.file_name, function (error, results) {
             if (error) {
                 return error;
             } else {
-                this.data.file_name  = name;
+                //this.data.file_name  = name;
+                
                 this.data = JSON.parse(results);
+                this.data.file_name = thisFileName;
                 if (!this.data.url||!this.data.webUrl) {
                     thisObit = this;
-                    client.makeUrl(name, {downloadHack: true}, function (error, result) {
+                    client.makeUrl(this.data.file_name, {downloadHack: true}, function (error, result) {
                         webUrl = "https://lathropd.github.com/reflowjs/?file="+result.url.replace(/https?\:\/\//,"");
                         $("#url").text(result.url).prop("href", result.url);
                         $("#webUrl").text(webUrl).prop("href",webUrl);
@@ -113,8 +149,8 @@ function obit(client, form, skybox) {
                 }
 
                 $("input, textarea").each(function (i, el) {
-                    if (this_obit.data.hasOwnProperty(el.id)) {
-                        el.value=this_obit.data[el.id];
+                    if (this_admin.data.hasOwnProperty(el.id)) {
+                        el.value=this_admin.data[el.id];
                     }
                 });
                 $("textarea.mdhtmlform-md").keyup();
@@ -124,20 +160,23 @@ function obit(client, form, skybox) {
 
     this.json = function () { return JSON.stringify(this.data);};
     this.write = function() {
-        this_obit = this;
-        client.writeFile(this.data.file_name, this.json(), function () {this_obit.loadEntries();});
+    	// create a variable we can pass to callbacks
+        this_admin = this;
+        
+        client.writeFile(this.data.file_name, this.json(), function () {this_admin.loadEntries();});
     }
     this.save = function () {
-        this_obit = this;
-
+    	// create a variable we can pass to callbacks
+        this_admin = this;
+        
         $(form).find("input, textarea").each(function (i, element) {
             if (element.name) {
-                if (!element.value&&this_obit.data.hasOwnProperty(element.name)) {
-                    this_obit.data[element.name] = "";
+                if (!element.value&&this_admin.data.hasOwnProperty(element.name)) {
+                    this_admin.data[element.name] = "";
                 } else if (element.value){
                     console.log(element.value);
-                    this_obit.data[element.name] = element.value.trim();
-                    this_obit.data[element.id] = element.value.trim();
+                    this_admin.data[element.name] = element.value.trim();
+                    this_admin.data[element.id] = element.value.trim();
                 }
             }
         });
@@ -147,7 +186,10 @@ function obit(client, form, skybox) {
         alert("Changes saved");
 
     }
-
+	
+	// load the initial set of entries
     this.loadEntries();
+    
+    // return the function as a JS object
     return this;
 }
